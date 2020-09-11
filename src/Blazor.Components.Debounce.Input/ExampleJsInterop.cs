15a2fc937d67ca5516cb1835dd35df52aa3bbaca
Major <major.timre@gmail.com>
2020-09-11 16:27:32 +0200

    Add basic infrastructure

---
+++ src/Blazor.Components.Debounce.Input/ExampleJsInterop.cs
@@ -0,0 +1,17 @@
+using Microsoft.JSInterop;
+
+using System.Threading.Tasks;
+
+namespace Blazor.Components.Debounce.Input
+{
+	public class ExampleJsInterop
+	{
+		public static ValueTask<string> Prompt(IJSRuntime jsRuntime, string message)
+		{
+			// Implemented in exampleJsInterop.js
+			return jsRuntime.InvokeAsync<string>(
+				"exampleJsFunctions.showPrompt",
+				message);
+		}
+	}
+}
