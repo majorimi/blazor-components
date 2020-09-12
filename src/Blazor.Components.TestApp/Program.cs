4a423a115f113aaaa8d8b9eff85be9bd35037c60
Major <major.timre@gmail.com>
2020-09-12 12:43:34 +0200

    Change debounce default behavior for min length

--- src/Blazor.Components.TestApp/Program.cs
+++ src/Blazor.Components.TestApp/Program.cs
@@ -19,6 +19,8 @@ namespace Blazor.Components.TestApp
 
 			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+			builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
 			await builder.Build().RunAsync();
 		}
 	}
