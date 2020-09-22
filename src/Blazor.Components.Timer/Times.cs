1459b9c86e11f76c74c4bb948783546d87100609
Imre Toth <majorimi@users.noreply.github.com>
2020-09-22 17:28:18 +0200

    Created AdvancedTimer component

---
+++ src/Blazor.Components.Timer/Times.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor.Components.Timer
+{
+	public record Times
+	{
+		public ulong Count { get; init; }
+
+		private Times() { }
+
+		public static Times Once() => new Times { Count = 1 };
+		public static Times Infinite() => new Times { Count = ulong.MaxValue };
+		public static Times Exactly(ulong count) => new Times { Count = count };
+	}
+}
