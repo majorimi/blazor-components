ac0fd88c2d7718480c8021cb095ba72426da32ab
Major <major.timre@gmail.com>
2020-09-22 22:52:24 +0200

    Added Blazor WebAssembly sample project code with installed nuget packages.

---
+++ samples/Blazor.Components.TestApp/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Blazor.WebAssembly.Logging.Console;
+
+namespace Blazor.Components.TestApp
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("app");
+
+			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			builder.Logging.AddBrowserConsole()
+				.SetMinimumLevel(LogLevel.Debug);
+
+			await builder.Build().RunAsync();
+		}
+	}
+}
