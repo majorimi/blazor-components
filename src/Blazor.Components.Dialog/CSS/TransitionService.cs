24f56f5eda2d22c69e11581dc868260247904738
Major <major.timre@gmail.com>
2020-10-05 19:26:47 +0200

    Fix blocking animations

---
+++ src/Blazor.Components.Dialog/CSS/TransitionService.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Blazor.Components.Dialog.CSS
+{
+	public class TransitionEventsService : IAsyncDisposable
+	{
+		private readonly IJSRuntime _jsRuntime;
+
+		private JSObjectReference _transitionJs;
+
+		public TransitionEventsService(IJSRuntime jsRuntime)
+		{
+			_jsRuntime = jsRuntime;
+		}
+
+		public async Task RegisterTransitionEnded(ElementReference elementRef, Func<TransitionEndEventArgs, Task> onEndedCallback, string transitionPropertyName = "")
+		{
+			await CheckJsObject();
+
+			var info = new TransitionInfo(elementRef, onEndedCallback);
+			var dotnetRef = DotNetObjectReference.Create<TransitionInfo>(info);
+
+			await _transitionJs.InvokeVoidAsync("addTransitionEnd", elementRef, dotnetRef, transitionPropertyName);
+		}
+
+		public async Task RemoveTransitionEnded(ElementReference elementRef, string transitionPropertyName = "")
+		{
+			await CheckJsObject();
+
+			await _transitionJs.InvokeVoidAsync("removeTransitionEnd", elementRef, transitionPropertyName);
+		}
+
+		public async Task RegisterTransitionsWhenAllEnded(Func<TransitionEndEventArgs[], Task> onEndedCallback, params KeyValuePair<ElementReference, string>[] elementRefsWithProperties)
+		{
+			await CheckJsObject();
+
+			var collection = new TransitionCollectionInfo(onEndedCallback);
+			foreach (var item in elementRefsWithProperties)
+			{
+				var info = new TransitionInfo(item.Key, collection.WhenAllFinished);
+				collection.Add(info);
+				var dotnetRef = DotNetObjectReference.Create<TransitionInfo>(info);
+
+				await _transitionJs.InvokeVoidAsync("addTransitionEnd", item.Key, dotnetRef, item.Value);
+			}
+		}
+
+		public async Task RemoveTransitionsWhenAllEnded(params KeyValuePair<ElementReference, string>[] elementRefsWithProperties)
+		{
+			await CheckJsObject();
+
+			foreach (var item in elementRefsWithProperties)
+			{
+				await _transitionJs.InvokeVoidAsync("removeTransitionEnd", item.Key, item.Value);
+			}
+		}
+
+		private async Task CheckJsObject()
+		{
+			if (_transitionJs is null)
+			{
+				_transitionJs = await _jsRuntime.InvokeAsync<JSObjectReference>("import", "./_content/Majorsoft.Blazor.Components.Dialog/transitionEvents.js");
+			}
+		}
+
+		public async ValueTask DisposeAsync()
+		{
+			if(_transitionJs is not null)
+			{
+				await _transitionJs.InvokeVoidAsync("dispose");
+
+				await _transitionJs.DisposeAsync();
+			}
+		}
+	}
+
+	public class TransitionCollectionInfo : Collection<TransitionInfo>
+	{
+		private readonly Func<TransitionEndEventArgs[], Task> _transitionEndedCallback;
+
+		public TransitionCollectionInfo(Func<TransitionEndEventArgs[], Task> transitionEndedCallback)
+		{
+			_transitionEndedCallback = transitionEndedCallback;
+		}
+
+		private List<TransitionEndEventArgs> _finishedTransitions = new List<TransitionEndEventArgs>();
+		public async Task WhenAllFinished(TransitionEndEventArgs args)
+		{
+			_finishedTransitions.Add(args);
+
+			if (this.Count == _finishedTransitions.Count)
+			{
+				await _transitionEndedCallback(_finishedTransitions.ToArray());
+				_finishedTransitions.Clear();
+			}
+		}
+	}
+
+	public class TransitionInfo
+	{
+		private readonly Func<TransitionEndEventArgs, Task> _transitionEndedCallback;
+		private readonly ElementReference _element;
+
+		public TransitionInfo(ElementReference element, Func<TransitionEndEventArgs, Task> transitionEndedCallback)
+		{
+			_element = element;
+			_transitionEndedCallback = transitionEndedCallback;
+		}
+
+		[JSInvokable("TransitionEnded")]
+		public async Task TransitionEnded(TransitionEndEventArgs args)
+		{
+			args.Element = _element;
+
+			await _transitionEndedCallback(args);
+		}
+	}
+
+	public class TransitionEndEventArgs
+	{
+		public ElementReference Element { get; set; }
+
+		public bool Composed { get; set; }
+		public double ElapsedTime { get; set; }
+		public int EventPhase { get; set; }
+		public string[] Path { get; set; }
+		public string PropertyName { get; set; }
+		public bool ReturnValue { get; set; }
+		public string Target { get; set; }
+		public string Type { get; set; }
+	}
+}
---
+++ src/Blazor.Components.Dialog/wwwroot/transitionEvents.js
@@ -0,0 +1,109 @@
+﻿function createEventHandler(dotnetRef, transitionPropertyName, invokeMethodName) {
+    let eventCallback = function (args) {
+        //console.log(args);
+        let name = args['propertyName'];
+
+        if (!transitionPropertyName || name === transitionPropertyName) {
+            let props = {
+                Composed: args.composed,
+                ElapsedTime: args.elapsedTime,
+                EventPhase: args.eventPhase,
+                //"Path": args.path,  //Throws serialization exception for Element
+                PropertyName: args.propertyName,
+                ReturnValue: args.returnValue,
+                //Target: args.target, //Throws serialization exception for Element
+                Type: args.type
+            }
+
+            dotnetRef.invokeMethodAsync(invokeMethodName, props);
+        }
+    }
+
+    return eventCallback;
+}
+
+let _transitionHandlerDict = [];
+
+export function addTransitionEnd(element, dotnetRef, transitionPropertyName) {
+    let eventCallback = createEventHandler(dotnetRef, transitionPropertyName, 'TransitionEnded');
+
+    let elementFound = false;
+    for (let i = 0; i < _transitionHandlerDict.length; i++) {
+        if (_transitionHandlerDict[i].key === element) {
+            if (_transitionHandlerDict[i].value) {
+                _transitionHandlerDict[i].value.push({ prop: transitionPropertyName, handler: eventCallback });
+            }
+            else {
+                _transitionHandlerDict[i].value = [{ prop: transitionPropertyName, handler: eventCallback }];
+            }
+
+            elementFound = true;
+            break;
+        }
+    }
+
+    if (!elementFound) {
+        _transitionHandlerDict.push({
+            key: element,
+            value: [{ prop: transitionPropertyName, handler: eventCallback }]
+        });
+    }
+
+    // Code for Safari 3.1 to 6.0
+    element.addEventListener("webkitTransitionEnd", eventCallback);
+    // Standard syntax
+    element.addEventListener("transitionend", eventCallback);
+}
+
+export function removeTransitionEnd(element, transitionPropertyName) {
+    let eventCallback;
+
+    for (let i = 0; i < _transitionHandlerDict.length; i++) {
+        if (_transitionHandlerDict[i].key === element) {
+            let val = _transitionHandlerDict[i].value;
+            for (let j = 0; i < val.length; j++) {
+                if (val[j].prop === transitionPropertyName) {
+                    eventCallback = val[j].handler; //associate callback
+                    _transitionHandlerDict[i].value.splice(j, 1); //remove handler
+                    break;
+                }
+            }
+
+            break;
+        }
+    }
+
+    if (!eventCallback) {
+        return; //No event handler found
+    }
+
+    // Code for Safari 3.1 to 6.0
+    element.removeEventListener("webkitTransitionEnd", eventCallback);
+    // Standard syntax
+    element.removeEventListener("transitionend", eventCallback);
+}
+
+export function dispose() {
+    _transitionHandlerDict = [];
+}
+
+//transitionend event args:
+/*
+bubbles: true
+cancelBubble: false
+cancelable: true
+composed: false
+currentTarget: null
+defaultPrevented: false
+elapsedTime: 1
+eventPhase: 0
+isTrusted: true
+path: (5)[div#myDIV, body, html, document, Window]
+propertyName: "width"
+pseudoElement: ""
+returnValue: true
+srcElement: div#myDIV
+target: div#myDIV
+timeStamp: 4927.3449999745935
+type: "transitionend"
+*/
