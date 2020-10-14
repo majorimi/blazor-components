c9bcf34d2f08b198d94b7bb8b959ac31fca5cb74
Major <major.timre@gmail.com>
2020-10-14 12:10:49 +0200

    Trying to fix Demo app

--- src/_copy.bat
+++ src/_copy.bat
@@ -9,4 +9,4 @@ xcopy .\Blazor.Components.TestApps.Common\Pages\ .\Blazor.Components.TestServerA
 rem DemoApp
 xcopy .\Blazor.Components.TestApps.Common\Pages\ ..\demo\Blazor.Components.DemoApp\Pages\ /E /Y
 xcopy .\Blazor.Components.TestApps.Common\Components\ ..\demo\Blazor.Components.DemoApp\Components\ /E /Y
-copy .\Blazor.Components.TestApps.Common\NavMenu.razor ..\demo\Blazor.Components.DemoApp\NavMenu.razor
+copy .\Blazor.Components.TestApps.Common\NavMenu.razor* ..\demo\Blazor.Components.DemoApp\Shared\
