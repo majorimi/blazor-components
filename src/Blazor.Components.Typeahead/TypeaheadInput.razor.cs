7ba4761d1871374f6402c49a06e92ac338dbe67f
Major <major.timre@gmail.com>
2020-10-26 19:52:26 +0100

    generic version

---
+++ src/Blazor.Components.Typeahead/TypeaheadInput.razor.cs
@@ -0,0 +1,13 @@
+﻿using Blazor.Components.Common.JsInterop.Click;
+
+using Microsoft.Extensions.Logging;
+
+namespace Blazor.Components.Typeahead
+{
+	public sealed partial class TypeaheadInput<TItem> : TypeaheadBase<TItem>
+	{
+		protected override ILogger BaseLogger => _logger;
+
+		protected override IClickBoundariesHandler ClickBoundariesHandler => _clickHandler;
+	}
+}
