54d220ab25da056e49078f6ef7b5064aabe4d8f8
Major <major.timre@gmail.com>
2020-10-27 14:08:40 +0100

    Updated Demo app

--- src/_copy.bat
+++ src/_copy.bat
@@ -10,4 +10,4 @@ rem DemoApp
 xcopy .\Blazor.Components.TestApps.Common\Pages\ ..\demo\Blazor.Components.DemoApp\Pages\ /E /Y
 xcopy .\Blazor.Components.TestApps.Common\Components\ ..\demo\Blazor.Components.DemoApp\Components\ /E /Y
 copy .\Blazor.Components.TestApps.Common\NavMenu.razor* ..\demo\Blazor.Components.DemoApp\Shared\
-copy .\Blazor.Components.TestApps.Common\_Imports.razor ..\demo\Blazor.Components.DemoApp\_Imports.razor /Y
+rem copy .\Blazor.Components.TestApps.Common\_Imports.razor ..\demo\Blazor.Components.DemoApp\_Imports.razor /Y
