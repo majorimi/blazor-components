32a1216e8fbc41a5150f696a5100656770664b8e
Major <major.timre@gmail.com>
2020-10-28 15:24:31 +0100

    Fixed mouse enter/leave event issues.

---
+++ src/Blazor.Components.PermaLink/EventHandlers.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Components.PermaLink
+{
+	[EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
+	[EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+	public static class EventHandlers
+	{
+	}
+}
