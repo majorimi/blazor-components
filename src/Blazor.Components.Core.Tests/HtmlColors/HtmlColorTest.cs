0b147f0a39de42c0f655c2c26e0a8cfae10e16f5
Major <major.timre@gmail.com>
2020-11-10 17:23:51 +0100

    HSL color changes and unit tests.

--- src/Blazor.Components.Core.Tests/HtmlColors/HtmlColorTest.cs
+++ src/Blazor.Components.Core.Tests/HtmlColors/HtmlColorTest.cs
@@ -1,4 +1,6 @@
-﻿using Blazor.Components.Core.HtmlColors;
+﻿using System;
+
+using Blazor.Components.Core.HtmlColors;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,32 +10,273 @@ namespace Blazor.Components.Core.Tests.HtmlColors
 	public class HtmlColorTest
 	{
 		[TestMethod]
-		public void HtmlColor_should_be_created_with_rgb_bytes()
+		public void HtmlColor_should_be_created_FromRgb_bytes()
 		{
-			var color = new HtmlColor((byte)200, (byte)55, (byte)27);
+			var color = HtmlColor.FromRgb(200, 55, 27);
 
 			Assert.IsNotNull(color);
 			Assert.AreEqual(null, color.OriginalValue);
 			Assert.AreEqual(false, color.IsNamedColor);
 			Assert.AreEqual(true, color.IsValid);
+
 			Assert.IsNotNull(color.RgbColor);
+			Assert.AreEqual(200, color.RgbColor.R);
+			Assert.AreEqual(55, color.RgbColor.G);
+			Assert.AreEqual(27, color.RgbColor.B);
+
+			Assert.IsNotNull(color.HslColor);
+			Assert.AreEqual(10, Math.Round(color.HslColor.Hue));
+			Assert.AreEqual(76, Math.Round(color.HslColor.Saturation));
+			Assert.AreEqual(45, Math.Round(color.HslColor.Luminosity));
+
 			Assert.AreEqual("#C8371B", color.RgbColor.ToHtmlHex());
 			Assert.AreEqual("#C8371B", color.HexColor);
 		}
 
 		[TestMethod]
-		public void HtmlColor_should_be_created_named_color_with_rgb_bytes()
+		public void HtmlColor_should_be_created_named_color_FromRgb_bytes()
 		{
-			var color = new HtmlColor((byte)255, (byte)0, (byte)0);
+			var color = HtmlColor.FromRgb(255, 0, 0);
 
 			Assert.IsNotNull(color);
 			Assert.AreEqual(null, color.OriginalValue);
 			Assert.AreEqual(true, color.IsNamedColor);
 			Assert.AreEqual("Red", color.ColorName);
 			Assert.AreEqual(true, color.IsValid);
+
 			Assert.IsNotNull(color.RgbColor);
+			Assert.AreEqual(255, color.RgbColor.R);
+			Assert.AreEqual(0, color.RgbColor.G);
+			Assert.AreEqual(0, color.RgbColor.B);
+
+			Assert.IsNotNull(color.HslColor);
+			Assert.AreEqual(360, Math.Round(color.HslColor.Hue));
+			Assert.AreEqual(100, Math.Round(color.HslColor.Saturation));
+			Assert.AreEqual(50, Math.Round(color.HslColor.Luminosity));
+
 			Assert.AreEqual("#FF0000", color.RgbColor.ToHtmlHex());
 			Assert.AreEqual("#FF0000", color.HexColor);
+		}
+
+		[TestMethod]
+		public void HtmlColor_should_be_created_FromRgb_string()
+		{
+			var color = HtmlColor.FromRgb("200, 55, 27");
+			var color2 = HtmlColor.FromRgb(null);
+			var color3 = HtmlColor.FromRgb("");
+			var color4 = HtmlColor.FromRgb("   ");
+			var color5 = HtmlColor.FromRgb("200,55,27");
+			var color6 = HtmlColor.FromRgb("200 55  27");
+			var color7 = HtmlColor.FromRgb("200 ,55  ,  27");
+
+			Assert.IsNotNull(color);
+			Assert.AreEqual("200, 55, 27", color.OriginalValue);
+			Assert.AreEqual(false, color.IsNamedColor);
+			Assert.AreEqual(true, color.IsValid);
+
+			Assert.IsNotNull(color.RgbColor);
+			Assert.AreEqual(200, color.RgbColor.R);
+			Assert.AreEqual(55, color.RgbColor.G);
+			Assert.AreEqual(27, color.RgbColor.B);
+
+			Assert.IsNotNull(color.HslColor);
+			Assert.AreEqual(10, Math.Round(color.HslColor.Hue));
+			Assert.AreEqual(76, Math.Round(color.HslColor.Saturation));
+			Assert.AreEqual(45, Math.Round(color.HslColor.Luminosity));
+
+			Assert.AreEqual("#C8371B", color.RgbColor.ToHtmlHex());
+			Assert.AreEqual("#C8371B", color.HexColor);
+
+			Assert.IsNotNull(color2);
+			Assert.AreEqual(false, color2.IsValid);
+			Assert.AreEqual(null, color2.HexColor);
+
+			Assert.IsNotNull(color3);
+			Assert.AreEqual(false, color3.IsValid);
+			Assert.AreEqual(null, color3.HexColor);
+
+			Assert.IsNotNull(color4);
+			Assert.AreEqual(false, color4.IsValid);
+			Assert.AreEqual(null, color4.HexColor);
+
+			Assert.IsNotNull(color5);
+			Assert.AreEqual(true, color5.IsValid);
+			Assert.AreEqual("#C8371B", color5.HexColor);
+
+			Assert.IsNotNull(color6);
+			Assert.AreEqual(true, color6.IsValid);
+			Assert.AreEqual("#C8371B", color6.HexColor);
+
+			Assert.IsNotNull(color7);
+			Assert.AreEqual(true, color7.IsValid);
+			Assert.AreEqual("#C8371B", color7.HexColor);
+		}
+
+		[TestMethod]
+		public void HtmlColor_should_be_created_FromHsl_ints()
+		{
+			var color = HtmlColor.FromHsl(10, 76, 45);
+
+			Assert.IsNotNull(color);
+			Assert.AreEqual(null, color.OriginalValue);
+			Assert.AreEqual(false, color.IsNamedColor);
+			Assert.AreEqual(true, color.IsValid);
+
+			Assert.IsNotNull(color.RgbColor); //Calculation differences...
+			Assert.AreEqual(202, color.RgbColor.R);
+			Assert.AreEqual(57, color.RgbColor.G);
+			Assert.AreEqual(28, color.RgbColor.B);
+
+			Assert.IsNotNull(color.HslColor);
+			Assert.AreEqual(10, Math.Round(color.HslColor.Hue));
+			Assert.AreEqual(76, Math.Round(color.HslColor.Saturation));
+			Assert.AreEqual(45, Math.Round(color.HslColor.Luminosity));
+
+			Assert.AreEqual("#CA391C", color.RgbColor.ToHtmlHex());
+			Assert.AreEqual("#CA391C", color.HexColor);
+		}
+
+		[TestMethod]
+		public void HtmlColor_should_be_created_FromHsl_string()
+		{
+			var color = HtmlColor.FromHsl("10, 76, 45");
+			var color2 = HtmlColor.FromHsl(null);
+			var color3 = HtmlColor.FromHsl("");
+			var color4 = HtmlColor.FromHsl("    ");
+			var color5 = HtmlColor.FromHsl("10°, 76%, 45%");
+			var color6 = HtmlColor.FromHsl("10° 76% 45%");
+			var color7 = HtmlColor.FromHsl("10°,76%,45%");
+			var color8 = HtmlColor.FromHsl("10°76%45%");
+			var color9 = HtmlColor.FromHsl("10°,76,45%");
+			var color10 = HtmlColor.FromHsl("10,76,45%");
+			var color11 = HtmlColor.FromHsl("10°  76 ,45%");
+
+			Assert.IsNotNull(color);
+			Assert.AreEqual("10, 76, 45", color.OriginalValue);
+			Assert.AreEqual(false, color.IsNamedColor);
+			Assert.AreEqual(true, color.IsValid);
+
+			Assert.IsNotNull(color.RgbColor); //Calculation differences...
+			Assert.AreEqual(202, color.RgbColor.R);
+			Assert.AreEqual(57, color.RgbColor.G);
+			Assert.AreEqual(28, color.RgbColor.B);
+
+			Assert.IsNotNull(color.HslColor);
+			Assert.AreEqual(10, Math.Round(color.HslColor.Hue));
+			Assert.AreEqual(76, Math.Round(color.HslColor.Saturation));
+			Assert.AreEqual(45, Math.Round(color.HslColor.Luminosity));
+
+			Assert.AreEqual("#CA391C", color.RgbColor.ToHtmlHex());
+			Assert.AreEqual("#CA391C", color.HexColor);
+
+			Assert.IsNotNull(color2);
+			Assert.AreEqual(false, color2.IsValid);
+			Assert.AreEqual(null, color2.HexColor);
+
+			Assert.IsNotNull(color3);
+			Assert.AreEqual(false, color3.IsValid);
+			Assert.AreEqual(null, color3.HexColor);
+
+			Assert.IsNotNull(color4);
+			Assert.AreEqual(false, color4.IsValid);
+			Assert.AreEqual(null, color4.HexColor);
+
+			Assert.IsNotNull(color5);
+			Assert.AreEqual(true, color5.IsValid);
+			Assert.AreEqual("#CA391C", color5.HexColor);
+
+			Assert.IsNotNull(color6);
+			Assert.AreEqual(true, color6.IsValid);
+			Assert.AreEqual("#CA391C", color6.HexColor);
+
+			Assert.IsNotNull(color7);
+			Assert.AreEqual(true, color7.IsValid);
+			Assert.AreEqual("#CA391C", color7.HexColor);
+
+			Assert.IsNotNull(color8);
+			Assert.AreEqual(true, color8.IsValid);
+			Assert.AreEqual("#CA391C", color8.HexColor);
+
+			Assert.IsNotNull(color9);
+			Assert.AreEqual(true, color9.IsValid);
+			Assert.AreEqual("#CA391C", color9.HexColor);
+
+			Assert.IsNotNull(color10);
+			Assert.AreEqual(true, color10.IsValid);
+			Assert.AreEqual("#CA391C", color10.HexColor);
+
+			Assert.IsNotNull(color11);
+			Assert.AreEqual(true, color11.IsValid);
+			Assert.AreEqual("#CA391C", color11.HexColor);
+		}
+
+		[TestMethod]
+		public void HtmlColor_should_be_created_named_color_FromHsl_ints()
+		{
+			var color = HtmlColor.FromHsl(0, 100, 50);
+
+			Assert.IsNotNull(color);
+			Assert.AreEqual(null, color.OriginalValue);
+			Assert.AreEqual(true, color.IsNamedColor);
+			Assert.AreEqual("Red", color.ColorName);
+			Assert.AreEqual(true, color.IsValid);
+
+			Assert.IsNotNull(color.RgbColor);
+			Assert.AreEqual(255, color.RgbColor.R);
+			Assert.AreEqual(0, color.RgbColor.G);
+			Assert.AreEqual(0, color.RgbColor.B);
+
+			Assert.IsNotNull(color.HslColor);
+			Assert.AreEqual(0, Math.Round(color.HslColor.Hue));
+			Assert.AreEqual(100, Math.Round(color.HslColor.Saturation));
+			Assert.AreEqual(50, Math.Round(color.HslColor.Luminosity));
+
+			Assert.AreEqual("#FF0000", color.RgbColor.ToHtmlHex());
+			Assert.AreEqual("#FF0000", color.HexColor);
+		}
+
+		[TestMethod]
+		public void HtmlColor_should_be_created_FromHex()
+		{
+			var color = HtmlColor.FromHex("#ff32C5");
+			var color2 = HtmlColor.FromHex(null);
+			var color3 = HtmlColor.FromHex(string.Empty);
+			var color4 = HtmlColor.FromHex("   ");
+			var color5 = HtmlColor.FromHex("12345");
+			var color6 = HtmlColor.FromHex("ff32C5");
+
+			Assert.IsNotNull(color);
+			Assert.AreEqual("#ff32C5", color.OriginalValue);
+			Assert.AreEqual(false, color.IsNamedColor);
+			Assert.AreEqual(true, color.IsValid);
+
+			Assert.IsNotNull(color.RgbColor); //Calculation differences...
+			Assert.AreEqual(255, color.RgbColor.R);
+			Assert.AreEqual(50, color.RgbColor.G);
+			Assert.AreEqual(197, color.RgbColor.B);
+
+			Assert.IsNotNull(color.HslColor);
+			Assert.AreEqual(317, Math.Round(color.HslColor.Hue));
+			Assert.AreEqual(100, Math.Round(color.HslColor.Saturation));
+			Assert.AreEqual(60, Math.Round(color.HslColor.Luminosity));
+
+			Assert.AreEqual("#FF32C5", color.RgbColor.ToHtmlHex());
+			Assert.AreEqual("#FF32C5", color.HexColor);
+
+			Assert.IsNotNull(color2);
+			Assert.AreEqual(false, color2.IsValid);
+
+			Assert.IsNotNull(color3);
+			Assert.AreEqual(false, color3.IsValid);
+
+			Assert.IsNotNull(color4);
+			Assert.AreEqual(false, color4.IsValid);
+
+			Assert.IsNotNull(color5);
+			Assert.AreEqual(false, color5.IsValid);
+
+			Assert.IsNotNull(color6);
+			Assert.AreEqual(true, color6.IsValid);
 		}
 
 		[TestMethod]
--- src/Blazor.Components.Core/HtmlColors/HtmlColor.cs
+++ src/Blazor.Components.Core/HtmlColors/HtmlColor.cs
@@ -8,37 +8,19 @@ namespace Blazor.Components.Core.HtmlColors
 {
 	public record HtmlColor
 	{
-		public string OriginalValue { get; init; }
+		public string OriginalValue { get; private set; }
 
-		public Color RgbColor { get; init; }
-		public HslColor HslColor { get; init; }
-		public string ColorName { get; init; }
-		public string HexColor { get; init; }
+		public Color RgbColor { get; private set; }
+		public HslColor HslColor { get; private set; }
+		public string ColorName { get; private set; }
+		public string HexColor { get; private set; }
 
-		public bool IsValid => RgbColor != default 
+		public bool IsValid => RgbColor != default
 			&& (!string.IsNullOrWhiteSpace(ColorName) || !string.IsNullOrWhiteSpace(HexColor));
 
 		public bool IsNamedColor => !string.IsNullOrWhiteSpace(ColorName);
 
-		public HtmlColor(byte r, byte g, byte b)
-		{
-			RgbColor = Color.FromArgb(r, g, b);
-			HslColor = HslColor.FromRgb(RgbColor);
-			HexColor = RgbColor.ToHtmlHex();
-
-			ColorName = HtmlColorHelper.NamedHtmlColors
-				.SingleOrDefault(x => x.Value == RgbColor.ToHex()).Key;
-		}
-
-		public HtmlColor(int h, int s, int l)
-		{
-			HslColor = new HslColor(h, s, l);
-			RgbColor = (Color)HslColor;
-			HexColor = RgbColor.ToHtmlHex();
-
-			ColorName = HtmlColorHelper.NamedHtmlColors
-				.SingleOrDefault(x => x.Value == RgbColor.ToHex()).Key;
-		}
+		private HtmlColor() { }
 
 		public HtmlColor(string value)
 		{
@@ -59,58 +41,94 @@ namespace Blazor.Components.Core.HtmlColors
 				return;
 			}
 
-			RgbColor = HexToHtmlRgb(value);
-			if(RgbColor != default) //Hex color
+			var ret = HtmlColor.FromHex(value); //HEX
+			if(!ret.IsValid)
 			{
-				HexColor = RgbColor.ToHtmlHex();
-				HslColor = HslColor.FromRgb(RgbColor);
-				ColorName = HtmlColorHelper.NamedHtmlColors
-					.SingleOrDefault(x => x.Value == RgbColor.ToHex()).Key;
+				ret = HtmlColor.FromRgb(value); //RGB
 
-				return;
+				if (!ret.IsValid)
+				{
+					ret = HtmlColor.FromHsl(value); //HSL
+				}
 			}
-			
-			if(IsRgbColor(value)) //RGB
+
+			if(ret.IsValid)
+			{
+				RgbColor = ret.RgbColor;
+				HexColor = ret.HexColor;
+				HslColor = ret.HslColor;
+				ColorName = ret.ColorName;
+			}
+		}
+
+		public static HtmlColor FromHex(string value)
+		{
+			var ret = new HtmlColor();
+			ret.OriginalValue = value;
+			if (string.IsNullOrWhiteSpace(value))
+			{
+				return ret;
+			}
+
+			ret.RgbColor = HexToHtmlRgb(value);
+			if (ret.RgbColor != default) //Hex color
+			{
+				ret.HexColor = ret.RgbColor.ToHtmlHex();
+				ret.HslColor = HslColor.FromRgb(ret.RgbColor);
+				ret.ColorName = HtmlColorHelper.NamedHtmlColors
+					.SingleOrDefault(x => x.Value == ret.RgbColor.ToHex()).Key;
+			}
+
+			return ret;
+		}
+
+		public static HtmlColor FromRgb(string value)
+		{
+			var ret = new HtmlColor();
+			ret.OriginalValue = value;
+			if (string.IsNullOrWhiteSpace(value))
+			{
+				return ret;
+			}
+
+			if (IsRgbColor(value)) //RGB
 			{
 				value = value.Replace(' ', ',');
 				var parts = value.Split(',').Where(s => !string.IsNullOrEmpty(s) && s != ",")
 					.Select(s => byte.Parse(s.Trim()))
 					.ToArray();
 
-				RgbColor = Color.FromArgb(parts[0], parts[1], parts[2]);
-				HexColor = RgbColor.ToHtmlHex();
-				HslColor = HslColor.FromRgb(RgbColor);
+				ret.RgbColor = Color.FromArgb(parts[0], parts[1], parts[2]);
+				ret.HexColor = ret.RgbColor.ToHtmlHex();
+				ret.HslColor = HslColor.FromRgb(ret.RgbColor);
 
-				ColorName = HtmlColorHelper.NamedHtmlColors
-					.FirstOrDefault(x => x.Value == RgbColor.ToHex()).Key;
-
-				return;
+				ret.ColorName = HtmlColorHelper.NamedHtmlColors
+					.FirstOrDefault(x => x.Value == ret.RgbColor.ToHex()).Key;
 			}
 
-			var match = IsHslColor(value);
-			if (match.Success) //HSL
-			{
-				var parts = match.Groups.Values.Skip(1)
-					.Select(s => s.Value?.Trim())
-					.Where(x => !string.IsNullOrWhiteSpace(x) && x != "," && x != "%" && x != "°")
-					.Select(s => int.Parse(s.Trim()))
-					.ToArray();
-
-				HslColor = new HslColor(parts[0], parts[1], parts[2]);
-				RgbColor = (Color)HslColor;
-				HexColor = RgbColor.ToHtmlHex();
-
-				ColorName = HtmlColorHelper.NamedHtmlColors
-					.SingleOrDefault(x => x.Value == RgbColor.ToHex()).Key;
-			}
+			return ret;
 		}
-
-		public HtmlColor(string value, bool forceHsl)
-			: this(value)
+		public static HtmlColor FromRgb(byte r, byte g, byte b)
 		{
-			if(!forceHsl)
+			var ret = new HtmlColor();
+
+			ret.RgbColor = Color.FromArgb(r, g, b);
+			ret.HslColor = HslColor.FromRgb(ret.RgbColor);
+			ret.HexColor = ret.RgbColor.ToHtmlHex();
+
+			ret.ColorName = HtmlColorHelper.NamedHtmlColors
+				.SingleOrDefault(x => x.Value == ret.RgbColor.ToHex()).Key;
+
+			return ret;
+		}
+
+		public static HtmlColor FromHsl(string value)
+		{
+			var ret = new HtmlColor();
+			ret.OriginalValue = value;
+			if (string.IsNullOrWhiteSpace(value))
 			{
-				return;
+				return ret;
 			}
 
 			var match = IsHslColor(value);
@@ -122,16 +140,30 @@ namespace Blazor.Components.Core.HtmlColors
 					.Select(s => int.Parse(s.Trim()))
 					.ToArray();
 
-				HslColor = new HslColor(parts[0], parts[1], parts[2]);
-				RgbColor = (Color)HslColor;
-				HexColor = RgbColor.ToHtmlHex();
+				ret.HslColor = new HslColor(parts[0], parts[1], parts[2]);
+				ret.RgbColor = (Color)ret.HslColor;
+				ret.HexColor = ret.RgbColor.ToHtmlHex();
 
-				ColorName = HtmlColorHelper.NamedHtmlColors
-					.SingleOrDefault(x => x.Value == RgbColor.ToHex()).Key;
+				ret.ColorName = HtmlColorHelper.NamedHtmlColors
+					.SingleOrDefault(x => x.Value == ret.RgbColor.ToHex()).Key;
 			}
+
+			return ret;
+		}
+		public static HtmlColor FromHsl(int h, int s, int l)
+		{
+			var ret = new HtmlColor();
+			ret.HslColor = new HslColor(h, s, l);
+			ret.RgbColor = (Color)ret.HslColor;
+			ret.HexColor = ret.RgbColor.ToHtmlHex();
+			ret.ColorName = HtmlColorHelper.NamedHtmlColors
+				.SingleOrDefault(x => x.Value == ret.RgbColor.ToHex()).Key;
+
+			return ret;
 		}
 
-		private Color HexToHtmlRgb(string value)
+
+		private static Color HexToHtmlRgb(string value)
 		{
 			if (!IsHtmlHexColor(value))
 			{
@@ -147,13 +179,13 @@ namespace Blazor.Components.Core.HtmlColors
 			return Color.FromArgb(parts[0].Value, parts[1].Value, parts[2].Value);
 		}
 
-		private bool IsHtmlHexColor(string value)
+		private static bool IsHtmlHexColor(string value)
 		{
 			var regex = new Regex("^#?[0-9a-fA-F]{6}$", RegexOptions.Compiled | RegexOptions.Singleline);
 			return regex.IsMatch(value?.Trim());
 		}
 
-		private bool IsRgbColor(string value)
+		private static bool IsRgbColor(string value)
 		{
 			const string numberPattern = @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
 			const string separatorPattern = @"(\s*|\s*,{1}\s*)";
@@ -162,8 +194,7 @@ namespace Blazor.Components.Core.HtmlColors
 			return regex.IsMatch(value?.Trim());
 		}
 
-
-		private Match IsHslColor(string value)
+		private static Match IsHslColor(string value)
 		{
 			const string huePattern = @"(360|3[0-5][0-9]|[012]?[0-9][0-9]?)(\s*°)?";
 			const string percentagePattern = @"(100|[0]?[0-9][0-9]?)(\s*%)?";
@@ -173,7 +204,7 @@ namespace Blazor.Components.Core.HtmlColors
 			return regex.Match(value?.Trim());
 		}
 
-		private int? HexToInt(string hex)
+		private static int? HexToInt(string hex)
 		{
 			if (int.TryParse(hex, NumberStyles.HexNumber, null, out var intValue))
 			{
@@ -183,7 +214,7 @@ namespace Blazor.Components.Core.HtmlColors
 			return null;
 		}
 
-		private byte? HexToByte(string hex)
+		private static byte? HexToByte(string hex)
 		{
 			if (byte.TryParse(hex, NumberStyles.HexNumber, null, out var val))
 			{
