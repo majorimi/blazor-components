b7188651593cb24dbf0fa21143bb11b928ff0aba
Major <major.timre@gmail.com>
2020-11-21 13:55:09 +0100

    Permalink element test proj

---
+++ src/Blazor.Components.PermaLink.Tests/PermaLinkElementTest.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading.Tasks;
+
+using Blazor.Components.Common.JsInterop.Clipboard;
+
+using Bunit;
+using Bunit.TestDoubles;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+namespace Blazor.Components.PermaLink.Tests
+{
+	[TestClass]
+	public class PermaLinkElementTest
+	{
+		private Bunit.TestContext _testContext;
+		private Mock<IClipboardHandler> _clipboardJsMock;
+
+		[TestInitialize]
+		public void Init()
+		{
+			_testContext = new Bunit.TestContext();
+
+			var mock = new Mock<ILogger<PermaLinkElement>>();
+			_clipboardJsMock = new Mock<IClipboardHandler>();
+
+			_testContext.Services.AddMockJSRuntime(JSRuntimeMockMode.Strict);
+			_testContext.Services.Add(new ServiceDescriptor(typeof(ILogger<PermaLinkElement>), mock.Object));
+			_testContext.Services.Add(new ServiceDescriptor(typeof(IClipboardHandler), _clipboardJsMock.Object));
+		}
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			_testContext?.Dispose();
+		}
+
+		[TestMethod]
+		public void PermaLinkElement_should_rendered_correctly_html_attributes()
+		{
+			//Test does not work unit new bUnit version
+			//https://github.com/egil/bUnit/issues/231
+
+			//var rendered = _testContext.RenderComponent<PermaLinkElement>(
+			//	("title", "t"), //HTML attributes
+			//	("style", "style") //HTML attributes
+			//	);
+
+			//var input = rendered.Find("div");
+
+			//Assert.IsNotNull(input);
+			//input.MarkupMatches(@"<input id=""id1"" class=""form-control w-100"" />");
+		}
+	}
+}
--- src/Blazor.Components.PermaLink/EventHandlers.cs
+++ src/Blazor.Components.PermaLink/EventHandlers.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Components;
 
 namespace Blazor.Components.PermaLink
 {
+	/// <summary>
+	/// Event handler to fix blinking icon
+	/// https://stackoverflow.com/questions/63265941/blazor-3-1-nested-onmouseover-events
+	/// </summary>
 	[EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
 	[EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
 	public static class EventHandlers
