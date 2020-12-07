f40fe1ed6a59b5c67c9b65e34dbcf886ad4e8378
Major <major.timre@gmail.com>
2020-12-07 16:05:01 +0100

    Implemented ToggleSwitch.

---
+++ src/Blazor.Components.Toggle.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blazor.Components.Toggle.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
