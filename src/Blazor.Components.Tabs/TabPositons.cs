debb4033c7c57db0a4e7be96f6774c926234ee73
Imre Toth <majorimi@hotmail.com>
2020-12-17 21:15:51 +0100

    Tabs positions.

---
+++ src/Blazor.Components.Tabs/TabPositons.cs
@@ -0,0 +1,21 @@
+﻿namespace Blazor.Components.Tabs
+{
+	/// <summary>
+	/// Determines the positions of tabs.
+	/// </summary>
+	public enum TabPositons
+	{
+		/// <summary>
+		/// Left side
+		/// </summary>
+		Left = 0,
+		/// <summary>
+		/// Centered
+		/// </summary>
+		Center = 1,
+		/// <summary>
+		/// Right side
+		/// </summary>
+		Right = 2,
+	}
+}
