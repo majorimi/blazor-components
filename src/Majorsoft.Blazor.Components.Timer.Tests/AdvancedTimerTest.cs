6cdffd80091879c6d35751728cc7a72fce5bd459
Imre Toth <majorimi@hotmail.com>
2020-12-31 12:09:33 +0100

    Obsolate Timer pulic methods.

--- src/Majorsoft.Blazor.Components.Timer.Tests/AdvancedTimerTest.cs
+++ src/Majorsoft.Blazor.Components.Timer.Tests/AdvancedTimerTest.cs
@@ -54,6 +54,7 @@ namespace Majorsoft.Blazor.Components.Timer.Tests
 
 			rendered.MarkupMatches("");
 			Assert.AreEqual(1, count);
+			Assert.AreEqual(false, rendered.Instance.IsEnabled);
 		}
 
 		[TestMethod]
@@ -72,6 +73,25 @@ namespace Majorsoft.Blazor.Components.Timer.Tests
 
 			rendered.MarkupMatches("");
 			Assert.AreEqual(required, count);
+			Assert.AreEqual(false, rendered.Instance.IsEnabled);
+		}
+
+		[TestMethod]
+		public async Task AdvancedTimer_should_execute_infinite_times()
+		{
+			var debounceTime = 30;
+			var count = 0;
+
+			var rendered = _testContext.RenderComponent<AdvancedTimer>(parameters => parameters
+				.Add(p => p.DelayInMilisec, debounceTime)
+				.Add(p => p.Occurring, Times.Infinite())
+				.Add(p => p.OnIntervalElapsed, c => { count = (int)c; }));
+
+			await Task.Delay(debounceTime * (200)); //wait for debounce
+
+			rendered.MarkupMatches("");
+			Assert.IsTrue(count > 3);
+			Assert.AreEqual(true, rendered.Instance.IsEnabled);
 		}
 
 		[TestMethod]
@@ -89,6 +109,57 @@ namespace Majorsoft.Blazor.Components.Timer.Tests
 
 			rendered.MarkupMatches("");
 			Assert.AreEqual(0, count);
+			Assert.AreEqual(false, rendered.Instance.IsEnabled);
+		}
+
+		[TestMethod]
+		public async Task AdvancedTimer_should_start_when_IsEnabled_true()
+		{
+			var debounceTime = 30;
+			var count = 0;
+
+			var rendered = _testContext.RenderComponent<AdvancedTimer>(parameters => parameters
+				.Add(p => p.DelayInMilisec, debounceTime)
+				.Add(p => p.IsEnabled, false)
+				.Add(p => p.AutoStart, false)
+				.Add(p => p.OnIntervalElapsed, c => { count = (int)c; }));
+
+			await Task.Delay(debounceTime * 10); //wait for debounce
+
+			rendered.MarkupMatches("");
+			Assert.AreEqual(0, count);
+
+			rendered.SetParametersAndRender(parameters => parameters
+				.Add(p => p.IsEnabled, true)
+			);
+
+			await Task.Delay(debounceTime * 10); //wait for debounce
+			Assert.AreEqual(1, count);
+			Assert.AreEqual(false, rendered.Instance.IsEnabled);
+		}
+
+		[TestMethod]
+		public async Task AdvancedTimer_should_reset()
+		{
+			var debounceTime = 30;
+			var count = 0;
+
+			var rendered = _testContext.RenderComponent<AdvancedTimer>(parameters => parameters
+				.Add(p => p.DelayInMilisec, debounceTime)
+				.Add(p => p.Occurring, Times.Infinite())
+				.Add(p => p.OnIntervalElapsed, c => { count = (int)c; }));
+
+			await Task.Delay(debounceTime * (100)); //wait for debounce
+
+			rendered.MarkupMatches("");
+			Assert.IsTrue(count > 3);
+			Assert.AreEqual(true, rendered.Instance.IsEnabled);
+
+			rendered.Instance.Reset();
+			await Task.Delay(debounceTime * (10)); //wait for debounce
+
+			Assert.AreEqual(1, count);
+			Assert.AreEqual(true, rendered.Instance.IsEnabled);
 		}
 	}
 }
