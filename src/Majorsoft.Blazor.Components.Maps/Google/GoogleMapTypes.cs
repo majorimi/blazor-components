6f1a89289fda6162221c04d50dbb11e954360cb3
Imre Toth <majorimi@hotmail.com>
2021-01-18 19:29:43 +0100

    Simple maps and Maps demo.

--- src/Majorsoft.Blazor.Components.Maps/Google/GoogleMapTypes.cs
+++ src/Majorsoft.Blazor.Components.Maps/Google/GoogleMapTypes.cs
@@ -22,17 +22,4 @@
 		/// </summary>
 		Hybrid = 3
 	}
-
-	/// <summary>
-	/// Google maps images may be returned in several common web graphics formats
-	/// </summary>
-	public enum GoogleMapImageFormats
-	{
-		Png = 0,
-		Png8 = 1,
-		Png32 = 2,
-		Gif = 3,
-		Jpg = 4,
-		//Jpg-baseline = 5,
-	}
 }
