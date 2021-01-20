377d7d4fcfd34f8426655e2400c3c6365f21ee8e
Imre Toth <majorimi@hotmail.com>
2021-01-20 20:42:20 +0100

    Write unit tests for GoogleSimpleMaps.

---
+++ src/Majorsoft.Blazor.Components.Maps.Tests/Google/GoogleMapMarkerCustomIconTest.cs
@@ -0,0 +1,35 @@
+﻿using Majorsoft.Blazor.Components.Maps.Google;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Majorsoft.Blazor.Components.Maps.Tests.Google
+{
+	[TestClass]
+	public class GoogleMapMarkerCustomIconTest
+	{
+		[TestMethod]
+		public void GoogleMapMarkerCustomIcon_should_handle_null()
+		{
+			var icon = new GoogleMapMarkerCustomIcon();
+
+			Assert.IsNotNull(icon.ToString());
+			Assert.AreEqual("", icon.ToString());
+		}
+
+		[TestMethod]
+		public void GoogleMapMarkerCustomIcon_should_generate_valid_string()
+		{
+			var icon = new GoogleMapMarkerCustomIcon()
+			{
+				IconUrl = "iconUrl"
+			}; var icon2 = new GoogleMapMarkerCustomIcon()
+			{
+				Anchor = GoogleMapMarkerCustomIconAnchors.Center,
+				IconUrl = "iconUrl"
+			};
+
+			Assert.AreEqual("anchor:top%7Cicon:iconUrl", icon.ToString());
+			Assert.AreEqual("anchor:center%7Cicon:iconUrl", icon2.ToString());
+		}
+	}
+}
---
+++ src/Majorsoft.Blazor.Components.Maps.Tests/Google/GoogleMapMarkerStyleTest.cs
@@ -0,0 +1,91 @@
+﻿using Majorsoft.Blazor.Components.Maps.Google;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Majorsoft.Blazor.Components.Maps.Tests.Google
+{
+	[TestClass]
+	public class GoogleMapMarkerStyleTest
+	{
+		[TestMethod]
+		public void GoogleMapMarkerStyle_should_handle_null()
+		{
+			var style = new GoogleMapMarkerStyle();
+
+			Assert.IsNotNull(style.ToString());
+			Assert.AreEqual("", style.ToString());
+		}
+
+		[TestMethod]
+		public void GoogleMapMarkerStyle_should_generate_only_size()
+		{
+			var style = new GoogleMapMarkerStyle()
+			{
+				Size = GoogleMapMarkerSizes.Mid
+			};
+
+			Assert.AreEqual("size:mid", style.ToString());
+		}
+
+		[TestMethod]
+		public void GoogleMapMarkerStyle_should_generate_only_color()
+		{
+			var style = new GoogleMapMarkerStyle()
+			{
+				Color = "white"
+			};
+
+			Assert.AreEqual("color:white", style.ToString());
+		}
+
+		[TestMethod]
+		public void GoogleMapMarkerStyle_should_generate_only_label()
+		{
+			var style = new GoogleMapMarkerStyle()
+			{
+				Label = 'a',
+			};
+
+			Assert.AreEqual("label:A", style.ToString());
+		}
+
+		[TestMethod]
+		public void GoogleMapMarkerStyle_should_generate_only_scale()
+		{
+			var style = new GoogleMapMarkerStyle()
+			{
+				Scale = 4
+			};
+
+			Assert.AreEqual("scale:4", style.ToString());
+		}
+
+		[TestMethod]
+		public void GoogleMapMarkerStyle_should_generate_only_non_default()
+		{
+			var style = new GoogleMapMarkerStyle()
+			{
+				Label = '9',
+				Color = "12345",
+				Scale = 1,
+				Size = GoogleMapMarkerSizes.Normal,
+			};
+
+			Assert.AreEqual("color:12345%7Clabel:9", style.ToString());
+		}
+
+		[TestMethod]
+		public void GoogleMapMarkerStyle_should_generate_only_all()
+		{
+			var style = new GoogleMapMarkerStyle()
+			{
+				Color = "red",
+				Size = GoogleMapMarkerSizes.Tiny,
+				Label = 'X',
+				Scale = 4
+			};
+
+			Assert.AreEqual("scale:4%7Csize:tiny%7Ccolor:red%7Clabel:X", style.ToString());
+		}
+	}
+}
