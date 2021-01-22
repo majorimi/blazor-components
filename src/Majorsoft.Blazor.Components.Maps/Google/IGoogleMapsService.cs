f6fc7c78a610fcd38b05474ea46c5df927d227cf
Imre Toth <majorimi@hotmail.com>
2021-01-22 17:54:41 +0100

    Google JS maps multi instance.

---
+++ src/Majorsoft.Blazor.Components.Maps/Google/IGoogleMapsService.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.JSInterop;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majorsoft.Blazor.Components.Maps.Google
+{
+	/// <summary>
+	/// GoogleMapsEventInfo event <see cref="DotNetObjectReference"/> info to handle Google Maps event callbacks
+	/// </summary>
+	internal sealed class GoogleMapsEventInfo
+	{
+		private readonly string _mapContainerId;
+		private readonly Func<string, Task> _mapInitializedCallback;
+
+		public GoogleMapsEventInfo(string mapContainerId, 
+			Func<string, Task> mapInitializedCallback = null)
+		{
+			_mapContainerId = mapContainerId;
+			_mapInitializedCallback = mapInitializedCallback;
+		}
+
+		[JSInvokable("MapInitialized")]
+		public async Task MapInitialized(string mapContainerId)
+		{
+			if(_mapContainerId != mapContainerId)
+			{
+				throw new InvalidProgramException($"{nameof(MapInitialized)} method was called with invalid Map container Div Id: {mapContainerId}, expected Id isL {_mapContainerId}.");
+			}
+
+			if (_mapInitializedCallback is not null)
+			{
+				await _mapInitializedCallback.Invoke(mapContainerId);
+			}
+		}
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public interface IGoogleMapsService : IAsyncDisposable
+	{
+		string MapContainerId { get; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="apiKey"></param>
+		/// <param name="mapContainerId"></param>
+		/// <param name="mapInitializedCallback"></param>
+		/// <returns></returns>
+		Task InitMap(string apiKey, string mapContainerId, Func<string, Task> mapInitializedCallback = null);
+
+		Task SetCenter();
+	}
+
+	public sealed class GoogleMapsService : IGoogleMapsService
+	{
+		private readonly IJSRuntime _jsRuntime;
+		private IJSObjectReference _mapsJs;
+
+		public string MapContainerId { get; private set; }
+
+		public GoogleMapsService(IJSRuntime jsRuntime)
+		{
+			_jsRuntime = jsRuntime;
+		}
+
+		public async Task InitMap(string apiKey, string mapContainerId, Func<string, Task> mapInitializedCallback)
+		{
+			if(MapContainerId == mapContainerId)
+			{
+				return; //Prevent double init...
+			}
+
+			MapContainerId = mapContainerId;
+			await CheckJsObjectAsync();
+
+			var info = new GoogleMapsEventInfo(mapContainerId, mapInitializedCallback);
+			var dotnetRef = DotNetObjectReference.Create<GoogleMapsEventInfo>(info);
+
+			await _mapsJs.InvokeVoidAsync("init", apiKey, mapContainerId, dotnetRef);
+		}
+
+		private async Task CheckJsObjectAsync()
+		{
+			if (_mapsJs is null)
+			{
+#if DEBUG
+				_mapsJs = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Majorsoft.Blazor.Components.Maps/googleMaps.js");
+#else
+				_mapsJs = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Majorsoft.Blazor.Components.Maps/googleMaps.min.js");
+#endif
+			}
+		}
+
+		public async ValueTask DisposeAsync()
+		{
+			if (_mapsJs is not null)
+			{
+				await _mapsJs.DisposeAsync();
+			}
+		}
+
+		public async Task SetCenter()
+		{
+			await CheckJsObjectAsync();
+			await _mapsJs.InvokeVoidAsync("setCenter");
+		}
+	}
+}
--- src/Majorsoft.Blazor.Components.Maps/wwwroot/googleMaps.js
+++ src/Majorsoft.Blazor.Components.Maps/wwwroot/googleMaps.js
@@ -1,6 +1,24 @@
-﻿export function init(key, elementId) {
-	mapElementId = elementId;
+﻿export function init(key, elementId, dotnetRef) {
+	if (!key || !elementId || !dotnetRef) {
+		return;
+	}
 
+	storeElementIdWithDotnetRef(_mapsElementDict, elementId, dotnetRef); //Store maps
+
+	let scriptsIncluded = false;
+	let scriptTags = document.querySelectorAll('head > script');
+	scriptTags.forEach(scriptTag => {
+		if (scriptTag.getAttribute('src').startsWith("https://maps.googleapis.com/maps/api/js?key=")) {
+			scriptsIncluded = true;
+			return;
+		}
+	});
+
+	if (scriptsIncluded) { //Prevent adding JS scripts to page multiple times.
+		return;
+	}
+
+	//Inject required Google JS scripts to HTML (only once!)
 	let importedPoly = document.createElement('script');
 	importedPoly.src = "https://polyfill.io/v3/polyfill.min.js?features=default";
 	document.head.appendChild(importedPoly);
@@ -11,21 +29,68 @@
 	importedMaps.defer = true;
 	document.head.appendChild(importedMaps);
 }
-//Global function for callback
-window.initGoogleMaps = () => {
-	map = new google.maps.Map(document.getElementById(mapElementId), {
-		center: { lat: -34.397, lng: 150.644 },
-		zoom: 8,
-	});
 
-	//let infoWindow = new google.maps.InfoWindow();
+//Global function for Google Js callback
+window.initGoogleMaps = () => {
+	//var map = new google.maps.Map(document.getElementById(mapElementId), {
+	//	center: { lat: -34.397, lng: 150.644 },
+	//	zoom: 8,
+	//});
+
+	for (let i = 0; i < _mapsElementDict.length; i++) {
+		let elementId = _mapsElementDict[i].key;
+		let map = new google.maps.Map(document.getElementById(elementId), {});
+		_mapsElementDict[i].value.map = map;
+
+		_mapsElementDict[i].value.ref.invokeMethodAsync("MapInitialized", elementId);
+	}
 };
 
-let mapElementId;
-let map;
-
-export function recenter() {
-	if (map) {
-		map.setCenter({ lat: 17.397, lng: 49.644 });
+//Store elementId with .NET Ref
+function storeElementIdWithDotnetRef(dict, elementId, dotnetRef) {
+	let elementFound = false;
+	for (let i = 0; i < dict.length; i++) {
+		if (dict[i].key === elementId) {
+			return; //Element has been stored already
+		}
 	}
+
+	if (!elementFound) {
+		dict.push({
+			key: elementId,
+			value: { ref: dotnetRef, map: null }
+		});
+	}
+}
+//Remove elementId with data
+function removeElementIdWithDotnetRef(dict, elementId) {
+	for (let i = 0; i < dict.length; i++) {
+		if (dict[i].key === elementId) {
+			dict.splice(i, 1);
+			break;
+		}
+	}
+}
+//Return elementId with data
+function getElementIdWithDotnetRef(dict, elementId) {
+	for (let i = 0; i < dict.length; i++) {
+		if (dict[i].key === elementId) {
+			return dict[i].value;
+		}
+	}
+}
+let _mapsElementDict = [];
+
+//Google maps Features
+export function setCenter(elementId, latitude, longitude) {
+	if (elementId) {
+		let map = getElementIdWithDotnetRef(_mapsElementDict, elementId);
+		if (map) {
+			map.setCenter({ lat: 17.397, lng: 49.644 });
+		}
+	}
+}
+
+export function dispose(elementId) {
+	removeElementIdWithDotnetRef(elementId);
 }
