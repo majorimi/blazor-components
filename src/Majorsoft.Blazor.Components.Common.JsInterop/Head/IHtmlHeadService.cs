f01e1baca7e504f43840f05ca86d13f61e92ca91
Major <major.timre@gmail.com>
2021-02-21 22:34:58 +0100

    Implemented FavIcon features.

--- src/Majorsoft.Blazor.Components.Common.JsInterop/Head/IHtmlHeadService.cs
+++ src/Majorsoft.Blazor.Components.Common.JsInterop/Head/IHtmlHeadService.cs
@@ -1,9 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.JSInterop;
 
 namespace Majorsoft.Blazor.Components.Common.JsInterop.Head
 {
+	/// <summary>
+	/// The `link` tag defines the relationship between the current document and an external resource.
+	/// </summary>
+	public class HtmlHeadLinkTag
+	{
+		/// <summary>
+		/// Specifies how the element handles cross-origin requests
+		/// </summary>
+		public string Crossorigin { get; set; }
+
+		/// <summary>
+		/// Specifies the location of the linked document
+		/// </summary>
+		public string Href { get; set; }
+
+		/// <summary>
+		/// Specifies the language of the text in the linked document
+		/// </summary>
+		public string HrefLang { get; set; }
+
+		/// <summary>
+		/// Specifies on what device the linked document will be displayed
+		/// </summary>
+		public string Media { get; set; }
+
+		/// <summary>
+		/// Specifies which referrer to use when fetching the resource
+		/// </summary>
+		public string ReferrerPolicy { get; set; }
+
+		/// <summary>
+		/// Required. Specifies the relationship between the current document and the linked document
+		/// </summary>
+		public string Rel { get; set; }
+
+		/// <summary>
+		/// Specifies the size of the linked resource. Only for rel="icon"
+		/// </summary>
+		public string Sizes { get; set; }
+
+		/// <summary>
+		/// Defines a preferred or an alternate stylesheet
+		/// </summary>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// Specifies the media type of the linked document
+		/// </summary>
+		public string Type { get; set; }
+	}
+
 	/// <summary>
 	/// Injectable service to handle HTML Head Interops.
 	/// </summary>
@@ -21,50 +72,26 @@ namespace Majorsoft.Blazor.Components.Common.JsInterop.Head
 		/// <param name="title"></param>
 		/// <returns>Async Task</returns>
 		Task SetHtmlTitleAsync(string title);
-	}
 
-	public class HtmlHeadService : IHtmlHeadService
-	{
-		private readonly IJSRuntime _jsRuntime;
-		private IJSObjectReference _headJs;
+		/// <summary>
+		/// Returns all existing Link tags from HTML page Head by given Rel type filter.
+		/// </summary>
+		/// <param name="linkType">Allowed Link `rel` types</param>
+		/// <returns>Async Task</returns>
+		Task<IEnumerable<HtmlHeadLinkTag>> GetHtmlHeadLinkTagsAsync(HtmlHeadLinkTagRelTypes? linkType = null);
 
-		public HtmlHeadService(IJSRuntime jsRuntime)
-		{
-			_jsRuntime = jsRuntime;
-		}
+		/// <summary>
+		/// Returns all existing fav icon "rel=icon" tags from HTML page Head.
+		/// </summary>
+		/// <returns>Async Task</returns>
+		Task<IEnumerable<HtmlHeadLinkTag>> GetHtmlFavIconsAsync();
 
-		public async Task<string> GetHtmlTitleAsync()
-		{
-			await CheckJsObjectAsync();
-
-			return await _headJs.InvokeAsync<string>("getHtmlTitle");
-		}
-		public async Task SetHtmlTitleAsync(string title)
-		{
-			await CheckJsObjectAsync();
-
-			await _headJs.InvokeVoidAsync("setHtmlTitle", title);
-		}
-
-		private async Task CheckJsObjectAsync()
-		{
-			if (_headJs is null)
-			{
-#if DEBUG
-				_headJs = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Majorsoft.Blazor.Components.Common.JsInterop/head.js");
-#else
-				_headJs = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Majorsoft.Blazor.Components.Common.JsInterop/head.min.js");
-#endif
-			}
-		}
-
-		public async ValueTask DisposeAsync()
-		{
-			if (_headJs is not null)
-			{
-				await _headJs.DisposeAsync();
-
-			}
-		}
+		/// <summary>
+		/// Removes all existing fav icon "rel=icon" tags from HTML page Head and creates new tags from the given objects.
+		/// If you have multiple fav icon tags set in the Head first call <see cref="GetHtmlFavIconsAsync"/> method and change <see cref="HtmlHeadLinkTag.Href"/> values.
+		/// </summary>
+		/// <param name="favIcons">New fav icons to set</param>
+		/// <returns>Async Task</returns>
+		Task SetHtmlFavIconsAsync(IEnumerable<HtmlHeadLinkTag> favIcons);
 	}
 }
--- src/Majorsoft.Blazor.Components.Common.JsInterop/wwwroot/head.js
+++ src/Majorsoft.Blazor.Components.Common.JsInterop/wwwroot/head.js
@@ -7,12 +7,94 @@ export function setHtmlTitle(newTitle) {
 	}
 }
 
-//function checkIfTagExistsInHead() {
-//	let headTags = document.querySelectorAll('head');
-//	headTags.forEach(tag => {
-//		//if (scriptTag.getAttribute('src').startsWith("https://maps.googleapis.com/maps/api/js?key=")) {
-//		//	scriptsIncluded = true;
-//		//	return;
-//		//}
-//	});
-//}
+export function getAllLinkHeadTags(relType) {
+	let links = null;
+	if (relType) {
+		links = getTagsFromHead('link', tag => {
+			if (tag.rel === relType)
+				return true;
+			return false;
+		});
+	}
+	else {
+		links = getTagsFromHead('link');
+	}
+
+	if (links && links.length) {
+		let result = [];
+		links.forEach(link => {
+			let obj = {
+				Crossorigin: link.crossorigin,
+				Href: link.href,
+				HrefLang: link.hreflang,
+				Media: link.media,
+				ReferrerPolicy: link.referrerpolicy,
+				Rel: link.rel,
+				Sizes: link.sizes.toString(),
+				Title: link.title,
+				Type: link.type
+			};
+
+			result.push(obj);
+		});
+
+		return result;
+	}
+
+	return links;
+}
+
+export function setFavIconsHeadTags(favIcons) {
+	if (!favIcons && !favIcons.length) {
+		return;
+	}
+
+	let icons = getTagsFromHead('link', tag => {
+		if (tag.rel === 'icon')
+			return true;
+		return false;
+	});
+
+	if (icons && icons.length) {
+		icons.forEach(icon => {
+			document.head.removeChild(icon);
+		});
+	}
+
+	favIcons.forEach(icon => {
+		let newFavIcon = document.createElement('link');
+		newFavIcon.rel = 'icon';
+		newFavIcon.href = icon.href;
+		newFavIcon.sizes = icon.sizes;
+
+		document.head.appendChild(newFavIcon);
+	});
+}
+
+function getTagsFromHead(tagName, filter) {
+	if (!tagName)
+		return null;
+
+	let headTags = document.querySelectorAll('head > *');
+	let retTags = [];
+
+	for (let i = 0; i < headTags.length; i++) {
+		let tag = headTags[i];
+		if (tag.tagName == tagName.toUpperCase()) {
+			if (!filter || filter(tag)) {
+				retTags.push(tag);
+			}
+		}
+	};
+
+	return retTags;
+}
+function isTagExistsInHead(tagName, filter) {
+	let ret = getTagsFromHead(tagName, filter);
+
+	if (ret && ret.length) {
+		return true;
+	}
+
+	return false;
+}
