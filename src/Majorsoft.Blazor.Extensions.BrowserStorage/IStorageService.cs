98b10cfb3a050c39d2838192defdea4b0be8113a
Major <major.timre@gmail.com>
2021-03-02 21:24:06 +0100

    More implementation of Storage.

---
+++ src/Majorsoft.Blazor.Extensions.BrowserStorage/IStorageService.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Majorsoft.Blazor.Extensions.BrowserStorage
+{
+	/// <summary>
+	/// The Storage interface of the Web Storage API provides access to a particular domain's session or local storage. 
+	/// It allows, for example, the addition, modification, or deletion of stored data items.
+	/// https://developer.mozilla.org/en-US/docs/Web/API/Storage
+	/// </summary>
+	public interface IStorageService
+	{
+		/// <summary>
+		/// Returns an integer representing the number of data items stored in the Storage object.
+		/// </summary>
+		/// <returns>Number of objects</returns>
+		Task<int> GetLengthAsync();
+
+		/// <summary>
+		/// When invoked, will empty all keys out of the storage.
+		/// </summary>
+		/// <returns>Async Task</returns>
+		Task ClearAsync();
+
+		/// <summary>
+		/// When passed a key name, will return that key's value.
+		/// </summary>
+		/// <typeparam name="T">Type param</typeparam>
+		/// <param name="key">Storage key</param>
+		/// <returns>Stored T instance object by key</returns>
+		Task<T> GetItemAsync<T>(string key);
+
+		/// <summary>
+		/// When passed a key name, will return that key's value.
+		/// </summary>
+		/// <param name="key">Storage key</param>
+		/// <returns>Sored object as String</returns>
+		Task<string> GetItemAsStringAsync(string key);
+
+		/// <summary>
+		/// Returns Storage key name by the given index.
+		/// </summary>
+		/// <param name="index">Index of the key</param>
+		/// <returns>Storage key name by index</returns>
+		Task<string> GetKeyAsync(int index);
+
+		/// <summary>
+		/// Returns all keys in Storage.
+		/// </summary>
+		/// <returns>List of keys</returns>
+		Task<IEnumerable<string>> GetKeysAsync();
+
+		/// <summary>
+		/// Checks if given key exists as Storage key.
+		/// </summary>
+		/// <param name="key">Storage key</param>
+		/// <returns>True if key exists, otherwise False</returns>
+		Task<bool> ContainKeyAsync(string key);
+
+		/// <summary>
+		/// When passed a key name, will remove that key from the storage.
+		/// </summary>
+		/// <param name="key">Storage key</param>
+		/// <returns>Async Task</returns>
+		Task RemoveItemAsync(string key);
+
+		/// <summary>
+		/// When passed a key name and value, will add that key to the storage, or update that key's value if it already exists.
+		/// </summary>
+		/// <typeparam name="T">Type param</typeparam>
+		/// <param name="key">Storage key</param>
+		/// <param name="data">Object to store</param>
+		/// <returns>Async Task</returns>
+		Task SetItemAsync<T>(string key, T data);
+	}
+}
--- src/Majorsoft.Blazor.Extensions.BrowserStorage/StorageServiceBase.cs
+++ src/Majorsoft.Blazor.Extensions.BrowserStorage/StorageServiceBase.cs
@@ -8,11 +8,6 @@ using Microsoft.JSInterop;
 
 namespace Majorsoft.Blazor.Extensions.BrowserStorage
 {
-	public interface IStorageService
-	{
-		Task<int> GetLengthAsync();
-	}
-
 	public abstract class StorageServiceBase : IStorageService
 	{
 		private readonly IJSRuntime _jSRuntime;
@@ -28,12 +23,56 @@ namespace Majorsoft.Blazor.Extensions.BrowserStorage
 			_storageName = name.Substring(0,1).ToLower() + name.Substring(1);
 		}
 
+		public async Task ClearAsync() => await _jSRuntime.InvokeVoidAsync($"{_storageName}.clear");
+
+		public async Task<bool> ContainKeyAsync(string key) => await _jSRuntime.InvokeAsync<bool>($"{_storageName}.hasOwnProperty", key);
+
+		public async Task<string> GetItemAsStringAsync(string key)
+		{
+			if (string.IsNullOrEmpty(key))
+			{
+				throw new ArgumentException($"Argument {nameof(key)} cannot by NULL or Empty.");
+			}
+
+			return await _jSRuntime.InvokeAsync<string>($"{_storageName}.getItem", key);
+		}
+
+		public Task<T> GetItemAsync<T>(string key) => throw new NotImplementedException();
+		
+		public async Task<string> GetKeyAsync(int index) => await _jSRuntime.InvokeAsync<string>($"{_storageName}.key", index);
+
+		public async Task<IEnumerable<string>> GetKeysAsync()
+		{
+			var length = await GetLengthAsync();
+
+			var tasks = new List<Task<string>>();
+			for (int i = 0; i < length; i++)
+			{
+				tasks.Add(GetKeyAsync(i));
+			}
+
+			var keys = await Task.WhenAll(tasks);
+			return keys;
+		}
+
 		public async Task<int> GetLengthAsync() => await _jSRuntime.InvokeAsync<int>("eval", $"{_storageName}.length");
+
+		public async Task RemoveItemAsync(string key)
+		{
+			if (string.IsNullOrEmpty(key))
+			{
+				throw new ArgumentException($"Argument {nameof(key)} cannot by NULL or Empty.");
+			}
+
+			await _jSRuntime.InvokeVoidAsync($"{_storageName}.removeItem", key);
+		}
+
+		public Task SetItemAsync<T>(string key, T data) => throw new NotImplementedException();
 	}
 
-	public interface ILocalStorageService : IStorageService
-	{ }
-
+	/// <summary>
+	/// Implementation of <see cref="ILocalStorageService"/>
+	/// </summary>
 	public class LocalStorageService : StorageServiceBase, ILocalStorageService
 	{
 		public LocalStorageService(IJSRuntime jSRuntime) 
@@ -42,9 +81,9 @@ namespace Majorsoft.Blazor.Extensions.BrowserStorage
 		}
 	}
 
-	public interface ISessionStorageService : IStorageService
-	{ }
-
+	/// <summary>
+	/// Implementation of <see cref="ISessionStorageService"/>
+	/// </summary>
 	public class SessionStorageService : StorageServiceBase, ISessionStorageService
 	{
 		public SessionStorageService(IJSRuntime jSRuntime) 
