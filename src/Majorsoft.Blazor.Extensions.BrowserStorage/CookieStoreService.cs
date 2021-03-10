1b246c85fd1179fc2d4efc0ff3e6be3eac36776a
Imre Toth <majorimi@hotmail.com>
2021-03-10 19:11:50 +0100

    Implemented ICookieStoreService

---
+++ src/Majorsoft.Blazor.Extensions.BrowserStorage/CookieStoreService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.JSInterop;
+
+namespace Majorsoft.Blazor.Extensions.BrowserStorage
+{
+	/// <summary>
+	/// Default implementation of <see cref="ICookieStoreService"/>
+	/// </summary>
+	public class CookieStoreService: ICookieStoreService
+	{
+		private readonly IJSRuntime _jSRuntime;
+
+		public CookieStoreService(IJSRuntime jSRuntime)
+		{
+			_jSRuntime = jSRuntime;
+		}
+
+		public async Task<Cookie> GetAsync(string name)
+		{
+			var ret = await _jSRuntime.InvokeAsync<Cookie>("cookieStore.get", name);
+			return ret;
+		}
+
+		public async Task<IEnumerable<Cookie>> GetAllAsync() => await _jSRuntime.InvokeAsync<IEnumerable<Cookie>>("cookieStore.getAll");
+
+		public async Task SetAsync(Cookie cookie)
+		{
+			await _jSRuntime.InvokeVoidAsync("cookieStore.set", cookie);
+		}
+
+		public async Task DeleteAsync(string name) => await _jSRuntime.InvokeAsync<Cookie>("cookieStore.delete", name);
+	}
+}
--- src/Majorsoft.Blazor.Extensions.BrowserStorage/ICookieStoreService.cs
+++ src/Majorsoft.Blazor.Extensions.BrowserStorage/ICookieStoreService.cs
@@ -1,49 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-
-using Microsoft.JSInterop;
 
 namespace Majorsoft.Blazor.Extensions.BrowserStorage
 {
 	/// <summary>
-	/// 
+	/// The CookieStore interface of the Cookie Store API provides methods for getting and setting cookies.
+	/// This feature is available only in secure contexts (HTTPS), in some or all supporting browsers.
+	/// https://developer.mozilla.org/en-US/docs/Web/API/CookieStore
 	/// </summary>
 	public interface ICookieStoreService
 	{
+		/// <summary>
+		/// The get() method gets a single cookie with the given name.
+		/// </summary>
+		/// <param name="name">Cookie name</param>
+		/// <returns>Async Task</returns>
 		Task<Cookie> GetAsync(string name);
+
+		/// <summary>
+		/// The getAll() method gets all matching cookies.
+		/// </summary>
+		/// <returns>Async Task</returns>
 		Task<IEnumerable<Cookie>> GetAllAsync();
 
+		/// <summary>
+		/// The set() method sets a cookie with the given name and value.
+		/// </summary>
+		/// <param name="cookie">Cookie data to set</param>
+		/// <returns>Async Task</returns>
 		Task SetAsync(Cookie cookie);
 
+		/// <summary>
+		/// The delete() method deletes a cookie with the given name.
+		/// </summary>
+		/// <param name="name">Cookie name</param>
+		/// <returns>Async Task</returns>
 		Task DeleteAsync(string name);
-	}
-
-	public class CookieStoreService: ICookieStoreService
-	{
-		private readonly IJSRuntime _jSRuntime;
-
-		public CookieStoreService(IJSRuntime jSRuntime)
-		{
-			_jSRuntime = jSRuntime;
-		}
-
-		public async Task<Cookie> GetAsync(string name)
-		{
-			var ret = await _jSRuntime.InvokeAsync<Cookie>("cookieStore.get", name);
-			return ret;
-		}
-
-		public async Task<IEnumerable<Cookie>> GetAllAsync() => await _jSRuntime.InvokeAsync<IEnumerable<Cookie>>("cookieStore.getAll");
-
-		public async Task SetAsync(Cookie cookie)
-		{
-			await _jSRuntime.InvokeVoidAsync("cookieStore.set", cookie);
-		}
-
-		public async Task DeleteAsync(string name) => await _jSRuntime.InvokeAsync<Cookie>("cookieStore.delete", name);
 	}
 }
