c0b7241cae96e1447655089fb078d0c60cc5b6cd
Major <major.timre@gmail.com>
2021-03-28 13:26:26 +0200

    Implemented ScrollToPageTop component.

---
+++ src/Majorsoft.Blazor.Components.Common.JsInterop/Scroll/PageScrollHorizontalOrientation.cs
@@ -0,0 +1,17 @@
+﻿namespace Majorsoft.Blazor.Components.Common.JsInterop.Scroll
+{
+	/// <summary>
+	/// Page scroll elements orientation on page <see cref="ScrollToPageBottom"/> and <see cref="ScrollToPageTop"/> elements
+	/// </summary>
+	public enum PageScrollHorizontalOrientation
+	{
+		/// <summary>
+		/// Element placed to the Right side 
+		/// </summary>
+		Right,
+		/// <summary>
+		/// Element placed to the Left side 
+		/// </summary>
+		Left
+	}
+}
