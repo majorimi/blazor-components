6cd3348e2bbfef0ef694a0322e2095e45927e3ef
Major <major.timre@gmail.com>
2021-05-29 17:21:58 +0200

    Analytics fixes

--- src/Majorsoft.Blazor.Extensions.Analytics/Google/GoogleAnalyticsService.cs
+++ src/Majorsoft.Blazor.Extensions.Analytics/Google/GoogleAnalyticsService.cs
@@ -31,7 +31,7 @@ namespace Majorsoft.Blazor.Extensions.Analytics.Google
 		/// <param name="trackingId">Is an identifier that uniquely identifies the target for hits, such as a Google Analytics property</param>
 		/// <param name="configInfo">Is one or more optional parameter-value pairs</param>
 		/// <returns>Async ValueTask</returns>
-		ValueTask Config(string trackingId = null, Dictionary<string, object> configInfo = null);
+		ValueTask Config(string trackingId = "", Dictionary<string, object> configInfo = null);
 
 		/// <summary>
 		/// Allows you to get various values from gtag.js including values set with the set command.
@@ -39,7 +39,7 @@ namespace Majorsoft.Blazor.Extensions.Analytics.Google
 		/// <param name="fieldName">The name of the field to get.</param>
 		/// <param name="trackingId">Is an identifier that uniquely identifies the target for hits, such as a Google Analytics property</param>
 		/// <returns>Async ValueTask</returns>
-		ValueTask Get(string fieldName, string trackingId = null);
+		ValueTask Get(string fieldName, string trackingId = "");
 
 		/// <summary>
 		/// Allows you to set values that persist across all the subsequent gtag() calls on the page.
@@ -91,15 +91,15 @@ namespace Majorsoft.Blazor.Extensions.Analytics.Google
 			await module.InvokeVoidAsync("init", trackingId);
 		}
 
-		public async ValueTask Config(string trackingId = null, Dictionary<string, object> configInfo = null)
+		public async ValueTask Config(string trackingId = "", Dictionary<string, object> configInfo = null)
 		{
 			var module = await moduleTask.Value;
-			await module.InvokeVoidAsync("config", TrackingId, configInfo?.ToList());
+			await module.InvokeVoidAsync("config", string.IsNullOrWhiteSpace(trackingId) ? TrackingId : trackingId, configInfo?.ToList());
 		}
-		public async ValueTask Get(string fieldName, string trackingId = null)
+		public async ValueTask Get(string fieldName, string trackingId = "")
 		{
 			var module = await moduleTask.Value;
-			await module.InvokeVoidAsync("get", TrackingId, fieldName); //TODO: callback results
+			await module.InvokeVoidAsync("get", string.IsNullOrWhiteSpace(trackingId) ? TrackingId : trackingId, fieldName); //TODO: callback results
 		}
 		public async ValueTask Set(Dictionary<string, object> parameterValuePair = null)
 		{
