62e5b09a813f90c8fd4028367fc799dccc95ba50
Major <major.timre@gmail.com>
2021-07-14 17:31:41 +0200

    GDPR consent fixes and demo

--- src/Majorsoft.Blazor.Components.GdprConsent/GdprConsentExtension.cs
+++ src/Majorsoft.Blazor.Components.GdprConsent/GdprConsentExtension.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using Majorsoft.Blazor.Components.Common.JsInterop;
+using Majorsoft.Blazor.Components.CssEvents;
 using Majorsoft.Blazor.Extensions.BrowserStorage;
 
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +25,9 @@ namespace Majorsoft.Blazor.Components.GdprConsent
 			}
 
 			services.AddBrowserStorage();
+			services.AddJsInteropExtensions();
+			services.AddCssEvents();
+
 			services.AddTransient<IGdprConsentService, GdprConsentService>();
 			services.AddSingleton<IGdprConsentNotificationService, GdprConsentNotificationService>();
 
