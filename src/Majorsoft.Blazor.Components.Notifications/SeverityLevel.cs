ec6d51513408295811cd81a5ce56273281b7ae3c
Major <major.timre@gmail.com>
2021-07-20 22:09:40 +0200

    Notifications Severity.

---
+++ src/Majorsoft.Blazor.Components.Notifications/SeverityLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace Majorsoft.Blazor.Components.Notifications
+{
+	/// <summary>
+	/// Severity levels for <see cref="Toast"/> and <see cref="Alert"/> components.
+	/// </summary>
+	public enum SeverityLevel
+	{
+		Normal,
+		Info,
+		Success,
+		Warning,
+		Error
+	}
+}
