b3f04a86f8e90c26ef67a6ca86633252f2ea7aee
Major <major.timre@gmail.com>
2021-07-23 16:08:35 +0200

    More unit test issues.

--- src/Majorsoft.Blazor.Components.Notifications.Tests/AlertTest.cs
+++ src/Majorsoft.Blazor.Components.Notifications.Tests/AlertTest.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Moq;
+using Majorsoft.Blazor.Components.Timer;
 
 namespace Majorsoft.Blazor.Components.Notifications.Tests
 {
@@ -26,10 +27,12 @@ namespace Majorsoft.Blazor.Components.Notifications.Tests
 			_testContext = new Bunit.TestContext();
 
 			var mock = new Mock<ILogger<Alert>>();
+			var mock2 = new Mock<ILogger<AdvancedTimer>>();
 			_transitionMock = new Mock<ITransitionEventsService>();
 			//_focusHandlerMock = new Mock<IFocusHandler>();
 
 			_testContext.Services.Add(new ServiceDescriptor(typeof(ILogger<Alert>), mock.Object));
+			_testContext.Services.Add(new ServiceDescriptor(typeof(ILogger<AdvancedTimer>), mock2.Object));
 			_testContext.Services.Add(new ServiceDescriptor(typeof(ITransitionEventsService), _transitionMock.Object));
 			//_testContext.Services.Add(new ServiceDescriptor(typeof(IFocusHandler), _focusHandlerMock.Object));
 		}
@@ -41,7 +44,7 @@ namespace Majorsoft.Blazor.Components.Notifications.Tests
 		}
 
 		[TestMethod]
-		public void Alert_should_not_rendered_anything_until_opened()
+		public void Alert_should_not_rendered_anything_until_IsVisible()
 		{
 			var rendered = _testContext.RenderComponent<Alert>(
 				("id", "id1"), //HTML attributes
@@ -51,6 +54,37 @@ namespace Majorsoft.Blazor.Components.Notifications.Tests
 
 			Assert.AreEqual(false, rendered.Instance.IsVisible);
 			rendered.MarkupMatches("");
+		}
+
+		[TestMethod]
+		public async Task Alert_should_rendered_correctly_html_attributes_when_IsVisible()
+		{
+			var rendered = _testContext.RenderComponent<Alert>(
+				("id", "id1"), //HTML attributes
+				("title", "text") //HTML attributes
+				);
+
+			//Open
+			rendered.Instance.IsVisible = true;
+			rendered.Render();
+
+			var div = rendered.Find("div");
+			Assert.IsNotNull(div);
+			Assert.AreEqual(true, rendered.Instance.IsVisible);
+
+			rendered.WaitForAssertion(() => rendered.MarkupMatches(@"<div class=""balert-main bnotify-normal-primary"" style=""opacity: 1;"" tabindex=""750""  >
+	  <div class=""balert-body"" id=""id1"" title=""text"" >
+		<div >
+		  <div ></div>
+		  <div class=""balert-text"" ></div>
+		</div>
+		<button type=""button""  class=""close normal"" >
+		  <span aria-hidden=""true"" >×</span>
+		  <span class=""sr-only"" >Close</span>
+		</button>
+	  </div>
+	  <div class=""balert-progress primary start"" style=""transition: width 10s linear;"" ></div>
+	</div>"));
 		}
 	}
 }
