1ee4475288d2b7311c41ae69b4ad85748484f8c9
Major <major.timre@gmail.com>
2021-08-17 22:17:06 +0200

    Empty Majorsoft.Blazor.Components.Tooltips\Majorsoft.Blazor.Components.Tooltips proj.

---
+++ src/Majorsoft.Blazor.Components.Tooltips.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Majorsoft.Blazor.Components.Tooltips.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
