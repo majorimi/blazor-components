f6764ac4840f629b1dde9e1844686f6c3a0feb5e
Major <major.timre@gmail.com>
2021-08-23 14:49:08 +0200

    Collapse panel CSS cleanup.

---
+++ src/Majorsoft.Blazor.Components.Collapse.Tests/AccordionTest.cs
@@ -0,0 +1,15 @@
+using Majorsoft.Blazor.Components.CommonTestsBase;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Majorsoft.Blazor.Components.Collapse.Tests
+{
+	[TestClass]
+	public class AccordionTest : ComponentsTestBase<Accordion>
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
