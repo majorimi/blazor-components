1dd25734a9a4cea6f5996370b59cfe3be26b0c69
Major <major.timre@gmail.com>
2021-09-08 17:22:04 +0200

    Permalink service updates added event for perma value detection and update link.

--- src/Majorsoft.Blazor.Components.PermaLink/IPermaLinkWatcherService.cs
+++ src/Majorsoft.Blazor.Components.PermaLink/IPermaLinkWatcherService.cs
@@ -9,6 +9,24 @@ namespace Majorsoft.Blazor.Components.PermaLink
 	public interface IPermaLinkWatcherService : IDisposable
 	{
 		/// <summary>
+		/// Should scroll smoothly to the found permalink element or jump.
+		/// Note: smooth scroll on element level might not supported by all Browsers.
+		/// </summary>
+		bool SmoothScroll { get; set; }
+
+		/// <summary>
+		/// Event handler for parmalinks detected on navigation.
+		/// </summary>
+		public event EventHandler<PermalinkDetectedEventArgs> PermalinkDetected;
+
+		/// <summary>
+		/// Modify the current URL with given new peralink value and trigger navigation or just update browser History.
+		/// </summary>
+		/// <param name="newPermalink">New peramlink value, NULL will remove permalink part from URL</param>
+		/// <param name="doNotNavigate">False, will trigger a navigation. When true, just add new URL to the History</param>
+		void ChangePermalink(string? newPermalink, bool doNotNavigate);
+
+		/// <summary>
 		/// Starts a navigation watcher which will check for Permalinks in the URLs.
 		/// </summary>
 		void WatchPermaLinks();
--- src/Majorsoft.Blazor.Components.PermaLink/PermaLinkWatcherService.cs
+++ src/Majorsoft.Blazor.Components.PermaLink/PermaLinkWatcherService.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 using Majorsoft.Blazor.Components.Common.JsInterop.Scroll;
 
@@ -19,11 +20,17 @@ namespace Majorsoft.Blazor.Components.PermaLink
 		private readonly NavigationManager _navigationManager;
 		private readonly ILogger<IPermaLinkWatcherService> _logger;
 
-		public PermaLinkWatcherService(IScrollHandler scrollHandler, NavigationManager navigationManager, ILogger<IPermaLinkWatcherService> logger)
+		public event EventHandler<PermalinkDetectedEventArgs> PermalinkDetected;
+
+		public bool SmoothScroll { get; set; }
+
+		public PermaLinkWatcherService(IScrollHandler scrollHandler, NavigationManager navigationManager, ILogger<IPermaLinkWatcherService> logger, bool smoothScroll = false)
 		{
 			_scrollHandler = scrollHandler;
 			_navigationManager = navigationManager;
 			_logger = logger;
+
+			SmoothScroll = smoothScroll;
 		}
 
 		public void WatchPermaLinks()
@@ -39,14 +46,67 @@ namespace Majorsoft.Blazor.Components.PermaLink
 		private void HandleLocationChanged(object sender, LocationChangedEventArgs e)
 		{
 			_logger.LogDebug($"{nameof(PermaLinkWatcherService)} - {nameof(HandleLocationChanged)}: navigation happened new URL: {e.Location}");
-			var match = _poundRegex.Match(e.Location);
+			var perma = DetectPermalink(e.Location);
 
-			if(match.Success && match.Groups.Count == 2)
+			if(!string.IsNullOrWhiteSpace(perma))
+			{
+				_logger.LogDebug($"{nameof(PermaLinkWatcherService)} - {nameof(HandleLocationChanged)}: PermaLink found: {perma}");
+
+				if(PermalinkDetected is not null)
+				{
+					PermalinkDetected.Invoke(this, new PermalinkDetectedEventArgs(e, perma));
+				}	
+
+				_scrollHandler.ScrollToElementByNameAsync(perma, SmoothScroll);
+			}
+		}
+
+		public void ChangePermalink(string? newPermalink, bool doNotNavigate)
+		{
+			var perma = DetectPermalink(_navigationManager.Uri);
+			if (!string.IsNullOrWhiteSpace(perma))
+			{
+				if (newPermalink?.ToLower() == perma?.ToLower()) //same
+				{
+					return;
+				}
+
+				var newUri = _navigationManager.Uri.Replace($"#{perma}", "");
+				if (string.IsNullOrWhiteSpace(newPermalink)) //remove
+				{
+					_navigationManager.NavigateTo(newUri);
+					return;
+				}
+
+				SetBrowserUrl($"{newUri}#{newPermalink}", doNotNavigate);
+			}
+			else
+			{
+				SetBrowserUrl($"{_navigationManager.Uri}#{newPermalink}", doNotNavigate);
+			}
+		}
+
+		private void SetBrowserUrl(string uri, bool doNotNavigate)
+		{
+			if(doNotNavigate)
+			{
+				//TODO: history.pushState(null, '', url);
+				return;
+			}
+
+			_navigationManager.NavigateTo(uri, false);
+		}
+
+		private string DetectPermalink(string uri)
+		{
+			var match = _poundRegex.Match(uri);
+			if (match.Success && match.Groups.Count == 2)
 			{
 				var perma = match.Groups[1].Value;
-				_logger.LogDebug($"{nameof(PermaLinkWatcherService)} - {nameof(HandleLocationChanged)}: PermaLink found: {perma}");
-				_scrollHandler.ScrollToElementByNameAsync(perma);
+				return perma;
 			}
+
+			return null;
 		}
 
 		public void Dispose()
