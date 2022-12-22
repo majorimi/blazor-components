89ec6db68490f8739dd1f45412872340f018f493
cupsos <cupsos@gmail.com>
2022-12-22 11:00:55 +0900

    Fix #121

---
+++ src/Majorsoft.Blazor.Components.Notifications/HTML5/EpochTimestampDateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Majorsoft.Blazor.Components.Notifications
+{
+    internal class EpochTimestampDateTimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            long ms = reader.GetInt64();
+            return DateTimeOffset.FromUnixTimeMilliseconds(ms).DateTime;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            long ms = new DateTimeOffset(value).ToUnixTimeMilliseconds();
+            writer.WriteNumberValue(ms);
+        }
+    }
+}
